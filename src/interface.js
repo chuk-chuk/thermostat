195a3e5d4d6a426c6ce7cf85d5fe6721638f0ec9
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2017-08-02 17:07:04 +0100

    Initial javascript functionality

--- src/interface.js
+++ src/interface.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
+
   $('.temp_display').text(thermostat.getTemp());
 
   $('.decrease').on('click', function(){
     thermostat.decreaseTemp(1);
     $('.temp_display').text(thermostat.getTemp());
-  })
+  });
+
+  $('.increase').on('click', function(){
+    thermostat.increaseTemp(1);
+    $('.temp_display').text(thermostat.getTemp());
+  });
+
+  $('.reset').on('click', function(){
+    thermostat.reset();
+    $('.temp_display').text(thermostat.getTemp());
+  });
+
+  $('.psm_on').on('click', function(){
+    thermostat.setPowerSavingModeOn();
+    $('.psm_display').text('Power saving mode is on');
+  });
+
+  $('.psm_off').on('click', function(){
+    thermostat.setPowerSavingModeOff();
+    $('.psm_display').text('Power saving mode is off');
+  });
 });
